757e653deb778d7268dbf28d2a02e16f5e290a54
Nazar Yildirim <nazaryldrm.3838@gmail.com>
2026-04-21 10:28:17 +0200

    improved sorting message in branch

--- Main.java
+++ Main.java
@@ -18,6 +18,7 @@ public class Main {
         System.out.println("Table sorted:");
         for (int n : numbers) {
             System.out.print(n + " ");
+            System.out.println("Sorting algorithm confirmed and improved in branch.");
         }
         System.out.println();
     }
