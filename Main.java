12026bd44f81e8bfef4a224cbdc8881a45555b5e
Nazar Yildirim <nazaryldrm.3838@gmail.com>
2026-04-21 10:02:08 +0200

    added table and random values

--- Main.java
+++ Main.java
@@ -2,6 +2,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Git Homework Project Started!");
-        // Bir sonraki adımda buraya tablo ekleyeceğiz
+        // e maddesi: Tablo (Array) oluşturma
+        int[] numbers = new int[5];
+        System.out.println("Table created.");
+
+        // g maddesi: Rastgele değerlerle doldurma
+        System.out.println("Filling table with random values:");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = (int) (Math.random() * 100);
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
     }
 }
